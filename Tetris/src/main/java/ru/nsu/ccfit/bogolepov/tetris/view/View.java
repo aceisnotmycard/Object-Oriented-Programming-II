c5e8a7e5c340ed1759d577526cd18981740d0689
Sergey Bogolepov <aceisnotmycard@icloud.com>
2015-05-12 18:41:02 +0600

    Removed old implementation of Tetris

--- Tetris/src/main/java/ru/nsu/ccfit/bogolepov/tetris/view/View.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.ccfit.bogolepov.tetris.view;
-
-import ru.nsu.ccfit.bogolepov.tetris.model.Field;
-
-import java.util.Observer;
-
-public interface View extends Observer {
-    void run();
-}
